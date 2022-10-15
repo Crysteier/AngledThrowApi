469eda61fa95cffc08a259cfec6262254364090d
Adam Moricz <crysteier12345@gmail.com>
2022-10-15 18:11:45 +0200

    Update AngledThrowController.cs
    Added rounding for better looks.
    Fix for bad stepCount.

--- AngledThrowApi/AngledThrowApi/Controllers/AngledThrowController.cs
+++ AngledThrowApi/AngledThrowApi/Controllers/AngledThrowController.cs
@@ -21,28 +21,30 @@ namespace SikmyVrhApi.Controllers
             var result = new CalculatedAngledThrow();
 
             double angleRad = (angle * Math.PI) / 180;
-            double stepTime = 0.2;
+            double stepTime = 0.1;
             double g = 9.81;
 
-            double finishTime = ((2 * velocity * Math.Sin(angleRad) / g)) / stepTime;
+            double finishTime = ((2 * velocity * Math.Sin(angleRad) / g));
 
-            result.tds = (2 * velocity * Math.Sin(angleRad)) / g;
-            result.FinishTime = finishTime;
-            result.HighestPoint = ((velocity * velocity) * Math.Sin(angleRad) * Math.Sin(angleRad)) / (2 * g);
+            result.tds = Math.Round((2 * velocity * Math.Sin(angleRad)) / g,4);
+            result.FinishTime = Math.Round(finishTime, 4);
+            result.HighestPoint = Math.Round(((velocity * velocity) * Math.Sin(angleRad) * Math.Sin(angleRad)) / (2 * g),4);
 
-            for (int i = 0; i <= Math.Round(finishTime); i++)
+            for (double i = 0; i <= finishTime; i += stepTime)
             {
-                var step = stepTime * i;
+                var step = i;
                 double x = (velocity * step * Math.Cos(angleRad));
-                double y = ((velocity * step * Math.Sin(angleRad))) - ((g * step * step) / 2);
-                var coords = new Coordinatas(x, y);
+                double y = ((velocity * step * Math.Sin(angleRad))) - ((g * (step * step)) / 2);
+
+                var coords = new Coordinatas(Math.Round(x,4), Math.Round(y, 4));
                 result.StepCount++;
                 result.Coords.Add(coords);
             }
 
             double distance = ((velocity * velocity) * Math.Sin(2 * angleRad)) / g;
-            result.Coords.Add(new Coordinatas(distance, 0));
-            result.DistanceTraveled = distance;
+            result.Coords.Add(new Coordinatas(Math.Round(distance, 4), 0.0));
+            result.StepCount++;
+            result.DistanceTraveled = Math.Round(distance,4);
 
             return result;
         }
